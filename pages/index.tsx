c5aa6e33e35f8f7042ca48042473e5cbf218fdb9
Codyconway77 <codyconway77@gmail.com>
2021-12-19 19:27:43 -0800

    Added blur placeholder on Image components rendered in project cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -94,25 +94,25 @@ const Home: NextPage = () => {
           <div className={styles['project-card']}>
             <h3>Calculator App</h3>
             <a href='https://eager-euclid-acf984.netlify.app/' target={'_blank'} rel='noreferrer'>
-              <Image src={CalculatorPreview} alt="preview picture of a calculator website" className={styles['project-pic']} />
+              <Image src={CalculatorPreview} placeholder="blur" alt="preview picture of a calculator website" className={styles['project-pic']} />
             </a>  
           </div>
           <div className={styles['project-card']}>
             <h3>Pomodoro Clock</h3>
             <a href="https://react-fcc-pomodoro-clock.netlify.app/" target={'_blank'} rel='noreferrer'>
-              <Image src={PomodoroPreview} alt='preview picture of a pomodoro clock website' className={styles['project-pic']} />
+              <Image src={PomodoroPreview} placeholder="blur" alt='preview picture of a pomodoro clock website' className={styles['project-pic']} />
             </a>
           </div>
           <div className={styles['project-card']}>
             <h3>Randome Quote Machine</h3>
             <a href="https://quirky-random-quote-generator.netlify.app/" target={'_blank'} rel='noreferrer'>
-              <Image src={QuotePreview} alt='preview picture of a random quote website' className={styles['project-pic']} />
+              <Image src={QuotePreview} placeholder="blur" alt='preview picture of a random quote website' className={styles['project-pic']} />
             </a>
           </div>
           <div className={styles['project-card']}>
             <h3>Text to Markdown</h3>
             <a href="https://quirky-fcc-markdown-previewer.netlify.app/" target={'_blank'} rel='noreferrer'>
-              <Image src={MarkdownPreview} alt='preview picutre of a text to markdown website' className={styles['project-pic']} />
+              <Image src={MarkdownPreview} placeholder="blur" alt='preview picutre of a text to markdown website' className={styles['project-pic']} />
             </a>
           </div>
         </div>
